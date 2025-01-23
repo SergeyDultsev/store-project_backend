2e5451bac755dc07d6161b9c0d815f9ee4d4d21b
SergeyDultsev <121752321+Dultsev2004@users.noreply.github.com>
2025-01-23 10:21:40 +0700

    fix auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasApiTokens;
 
-    protected $primaryKey = 'string';
+    protected $primaryKey = 'id';
     protected $keyType = 'string';
     public $incrementing = false;
 
